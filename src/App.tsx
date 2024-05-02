0f1444467387e69786e57ab779ad4d287d74c0ee
Pedro Clerici <pedro.clerici@protonmail.com>
2024-05-02 17:00:07 -0300

    Added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
+
 export function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <>
+      <Header />
+
+      <main className={styles.wrapper}></main>
+    </>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src="/rocket.svg" alt="Logotipo do Ignite Todo" />
+      <h1>
+        to<span>do</span>
+      </h1>
+    </header>
+  );
+}
